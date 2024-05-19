0ad9ede7914ab48f494ef505994c27f49de3e2ea
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-05-19 14:56:26 +0400

    cookies

--- router/product.py
+++ router/product.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 
-from fastapi import APIRouter, status, Header
+from fastapi import APIRouter, status, Header, Cookie
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 
 router = APIRouter(
@@ -13,17 +13,26 @@ products = ["watch", "camera", "phone"]
 
 @router.get("/all")
 def get_all_products():
+    # return products
     data = " ".join(products)
-    return Response(content=data, media_type="text/plain")
+    response = Response(content=data, media_type="text/plain")
+    response.set_cookie(key="test_cookie", value="test_cookie_value")
+    return response
 
 
 @router.get("/with-header")
 def get_products(
         response: Response,
-        custom_header: Optional[List[str]] = (Header([])),
+        custom_header: Optional[List[str]] = Header(None),
+        test_cookie: Optional[str] = Cookie(None),
 ):
-    response.headers["custom-response-header"] = ", ".join(custom_header)
-    return products
+    if custom_header:
+        response.headers["custom-response-header"] = ", ".join(custom_header)
+    return {
+        "data": products,
+        "custom_header": custom_header,
+        "my_cookie": test_cookie
+    }
 
 
 @router.get("/{id}", responses={
